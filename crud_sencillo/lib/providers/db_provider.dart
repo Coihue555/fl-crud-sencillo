dd366c115b86a8d61088ad9bbf94bbb05b25a40e
Coihue555 <kevlar555@gmail.com>
2022-03-20 21:56:49 -0300

    textEditingController

--- crud_sencillo/lib/providers/db_provider.dart
+++ crud_sencillo/lib/providers/db_provider.dart
@@ -7,36 +7,28 @@ import 'package:sqflite/sqflite.dart';
 import 'package:crud_sencillo/models/dato_model.dart';
 export 'package:crud_sencillo/models/dato_model.dart';
 
-class DBProvider{
-
+class DBProvider {
   static Database? _database;
   static final DBProvider db = DBProvider._();
 
   DBProvider._();
 
   Future<Database?> get database async {
-    if( _database != null ) return _database;
+    if (_database != null) return _database;
 
     _database = await initDB();
 
     return _database;
-    
   }
-  
+
   Future<dynamic> initDB() async {
-
-    
-
     Directory documentsDirectory = await getApplicationDocumentsDirectory();
     final path = join(documentsDirectory.path, 'PersonasDB.db');
     print(path);
 
-    return await openDatabase(
-      path,
-      version: 1,
-      onOpen: (db){},
-      onCreate: (Database db, int version) async {
-        await db.execute('''
+    return await openDatabase(path, version: 1, onOpen: (db) {},
+        onCreate: (Database db, int version) async {
+      await db.execute('''
           CREATE TABLE Datos(
             id INTEGER PRIMARY KEY,
             nombre TEXT,
@@ -44,102 +36,100 @@ class DBProvider{
           )
 
         ''');
-      }
-    );
-
-    
+    });
   }
 
-  Future<int> nuevoDato( DatoModel nuevoDato) async {
-    final id    =  nuevoDato.id;
-    final nombre  = nuevoDato.nombre;
+  Future<int> nuevoDato(DatoModel nuevoDato) async {
+    final id = nuevoDato.id;
+    final nombre = nuevoDato.nombre;
     final email = nuevoDato.email;
 
-
     //verificar la db
-      final db = await database;
+    final db = await database;
 
-      final res = await db!.rawInsert('''
+    final res = await db!.rawInsert('''
       INSERT INTO Datos(id, nombre, email)
         VALUES( $id, '$nombre', '$email')
-      '''
-      );
+      ''');
 
-      return res;
-    }
-    
+    return res;
+  }
 
-    Future<int> nuevoDatoRaw(DatoModel nuevoDato) async {
-      final db = await database;
-      final res = await db!.insert('Datos', nuevoDato.toJson());
-      //id del ultimo registro
-      return res;
-    }
+  Future<int> nuevoDatoRaw(DatoModel nuevoDato) async {
+    final db = await database;
+    final res = await db!.insert('Datos', nuevoDato.toJson());
+    //id del ultimo registro
+    return res;
+  }
 
-    Future<DatoModel?> getDatosById(int id) async {
-      final db = await database;
-      final res = await db!.query('Datos', where: 'id = ?', whereArgs: [id]);
+  Future<DatoModel?> getDatosById(int id) async {
+    final db = await database;
+    final res = await db!.query('Datos', where: 'id = ?', whereArgs: [id]);
 
-      return res.isNotEmpty
-        ? DatoModel.fromJson(res.first)
-        : null;
-    }
+    return res.isNotEmpty ? DatoModel.fromJson(res.first) : null;
+  }
 
-    Future<List<DatoModel>?> getTodos() async {
-      final db = await database;
-      final res = await db!.query('Datos', orderBy: 'id DESC');
+  Future<List<DatoModel>?> getTodos() async {
+    final db = await database;
+    final res = await db!.query('Datos', orderBy: 'id DESC');
 
-      
-      return res.isNotEmpty
-        ? res.map((s) => DatoModel.fromJson(s)).toList()
-        : [];
-    }
+    return res.isNotEmpty ? res.map((s) => DatoModel.fromJson(s)).toList() : [];
+  }
 
-    Future<List<DatoModel>?> getDatosByNombre(String nombre) async {
-      final db = await database;
-      final res = await db!.rawQuery('''
+  Future<List<DatoModel>?> getDatosByNombre(String nombre) async {
+    final db = await database;
+    final res = await db!.rawQuery('''
         SELECT * FROM Datos WHERE nombre = '$nombre'
       ''');
 
-      return res.isNotEmpty
-        ? res.map((s) => DatoModel.fromJson(s)).toList()
-        : [];
-    }
+    return res.isNotEmpty ? res.map((s) => DatoModel.fromJson(s)).toList() : [];
+  }
 
-    Future<int> updateDato(DatoModel nuevoDato) async{
-      final db = await database;
-      final res = await db!.update('Datos', nuevoDato.toJson(), where: 'id = ?', whereArgs: [nuevoDato.id]);
+  Future<int> updateDato(DatoModel nuevoDato) async {
+    final db = await database;
+    final res = await db!.update('Datos', nuevoDato.toJson(),
+        where: 'id = ?', whereArgs: [nuevoDato.id]);
 
-      return res;
+    return res;
+  }
 
-    }
+  Future<int> updateItem(int id, String nombre, String? email) async {
+    final db = await database;
 
-    Future<DatoModel?> getScansById(int id) async {
-      final db = await database;
-      final res = await db!.query('Datos', where: 'id = ?', whereArgs: [id]);
+    final data = {
+      'nombre': nombre,
+      'email': email,
+    };
 
-      return res.isNotEmpty
-        ? DatoModel.fromJson(res.first)
-        : null;
-    }
+    final result =
+        await db!.update('items', data, where: "id = ?", whereArgs: [id]);
+    return result;
+  }
 
-    Future<int> updateScan(DatoModel nuevoScan) async{
-      final db = await database;
-      final res = await db!.update('Datos', nuevoScan.toJson(), where: 'id = ?', whereArgs: [nuevoScan.id]);
+  Future<DatoModel?> getScansById(int id) async {
+    final db = await database;
+    final res = await db!.query('Datos', where: 'id = ?', whereArgs: [id]);
 
-      return res;
+    return res.isNotEmpty ? DatoModel.fromJson(res.first) : null;
+  }
 
-    }
+  Future<int> updateScan(DatoModel nuevoScan) async {
+    final db = await database;
+    final res = await db!.update('Datos', nuevoScan.toJson(),
+        where: 'id = ?', whereArgs: [nuevoScan.id]);
 
-    Future<int> deleteDato( int id) async{
-      final db = await database;
-      final res = await db!.delete('Datos', where: 'id = ?', whereArgs: [id]);
-      return res;
-    }
+    return res;
+  }
 
-    Future<int> deleteAllDatos( ) async{
-      final db = await database;
-      final res = await db!.delete('Datos');
-      return res;
-    }
+  Future<int> deleteDato(int id) async {
+    final db = await database;
+    final res = await db!.delete('Datos', where: 'id = ?', whereArgs: [id]);
+    return res;
+  }
+
+  Future<int> deleteAllDatos() async {
+    final db = await database;
+    final res = await db!.delete('Datos');
+    return res;
+  }
 }
--- crud_sencillo/lib/providers/persona_list_provider.dart
+++ crud_sencillo/lib/providers/persona_list_provider.dart
@@ -1,20 +1,21 @@
 import 'package:flutter/material.dart';
-import 'package:crud_sencillo/models/dato_model.dart';
 import 'package:crud_sencillo/providers/db_provider.dart';
 
-class DatosListProvider extends ChangeNotifier{
+class DatosListProvider extends ChangeNotifier {
   List<DatoModel> datos = [];
 
+  final TextEditingController _nombreController = TextEditingController();
+  final TextEditingController _emailController = TextEditingController();
+
   Future<DatoModel> nuevoDato(String nombre, String email) async {
-    final nuevoDato =  DatoModel(nombre: nombre, email:email);
+    final nuevoDato = DatoModel(nombre: nombre, email: email);
     final id = await DBProvider.db.nuevoDato(nuevoDato);
     //asignar el ID de la base de datos al modelo
     nuevoDato.id = id;
 
-    
-      datos.add(nuevoDato);
-      notifyListeners();
-        
+    datos.add(nuevoDato);
+    notifyListeners();
+
     return nuevoDato;
   }
 
@@ -23,19 +24,18 @@ class DatosListProvider extends ChangeNotifier{
     this.datos = [...datos!];
     notifyListeners();
     return datos;
-
-
   }
-  
 
-  Future<int> updateDato(nombre, email) async{
+  Future<void> _actualizarItem(int id) async {
+    await DBProvider.db
+        .updateItem(id, _nombreController.text, _emailController.text);
+    notifyListeners();
+  }
+
+  Future<int> updateDato(nombre, email) async {
     final datos = await DBProvider.db.updateDato(nombre);
-    
+
     notifyListeners();
     return datos;
-
   }
-
-
-
 }
